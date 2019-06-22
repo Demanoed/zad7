a92a1e223f11e67bffb24726fbcd6d94e47407ea
Трефилов Дмитрий <trefilowdem@mail.ru>
2019-06-22 18:29:59 +0500

    Add library DemanT_Library and function Request

--- PovtorElem/PovtorElem/Program.cs
+++ PovtorElem/PovtorElem/Program.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DemanT_Library;
 
 namespace PovtorElem
 {
     class Program
     {
+        private static int n;
+        private static int[] elements;
+        private static void Request()
+        {
+
+        }
         static void Main()
         {
 
