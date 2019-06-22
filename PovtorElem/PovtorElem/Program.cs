ad30e2d40d6a84d54cf76d9514ae599adc26a45b
Трефилов Дмитрий <trefilowdem@mail.ru>
2019-06-22 18:39:23 +0500

    Update function Request

--- PovtorElem/PovtorElem/Program.cs
+++ PovtorElem/PovtorElem/Program.cs
@@ -13,11 +13,18 @@ namespace PovtorElem
         private static int[] elements;
         private static void Request()
         {
-
+            ColorMess.Yellow("\n Введите колличество элементов из скольки будем генерировать варианты (от 2 до 5): ");
+            n = Input.Check(2, 10);
+            for(int i = 0; i<n; i++)
+            {
+                Console.Clear();
+                ColorMess.Yellow("\n Введите " + (i+1) + " число (от 0 до 9): ");
+                elements[i] = Input.Check(0, 9);
+            }
         }
         static void Main()
         {
-
+            Request();
         }
     }
 }
