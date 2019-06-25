fa9f02f4589cc89f8d5c993df0025d68fa37f4a6
Дмитрий Трефилов <trefilowdem@mail.ru>
2019-06-26 00:51:14 +0500

    Add comments

--- PovtorElem/PovtorElem/Program.cs
+++ PovtorElem/PovtorElem/Program.cs
@@ -9,8 +9,11 @@ namespace PovtorElem
 {
     class Program
     {
+        #region Переменные для колличества элемнтов и чисел, а также массив для их хранения
         private static int n, k;
         private static int[] elements;
+        #endregion
+        #region Запрос колличества чисел и элементов
         private static void Request()
         {
             ColorMess.Yellow("\n Введите колличество элементов из скольки будем генерировать варианты (от 2 до 9): ");
@@ -18,12 +21,16 @@ namespace PovtorElem
             ColorMess.Yellow("\n Сколько будет чисел (от 2 до 9): ");
             k = Input.Check(2, 9);
         }
+        #endregion
+        #region Печать чисел
         private static void PrintElements()
         {
             for (int i = 0; i < n; i++)
                 ColorMess.White(" " + elements[i]);
             Console.WriteLine();
         }
+        #endregion
+        #region Вычисление факториала числа
         private static int Factorial(int number)
         {
             int res = 1;
@@ -31,10 +38,14 @@ namespace PovtorElem
                 res *= i;
             return res;
         }
+        #endregion
+        #region Вычисление общего колличества чисел по формуле
         private static int KolvoChisel()
         {
             return (Factorial(k+n-1)) / (Factorial(n-1)*Factorial(k));
         }
+        #endregion
+        #region Генерация всех чисел в лексикографическом порядке
         private static void GenerateElements()
         {
             elements = new int[n];
@@ -61,6 +72,8 @@ namespace PovtorElem
                 }
             }
         }
+        #endregion
+        #region Запрашивает у пользователя ввод данных и генерирует числа
         static void Main()
         {
             Request();
@@ -69,5 +82,6 @@ namespace PovtorElem
             GenerateElements();
             Message.GoToBack();
         }
+        #endregion
     }
 }
